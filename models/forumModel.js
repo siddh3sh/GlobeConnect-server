7777deaf13edaa53d0fa9fd6f3d74de63e098aff
siddh3sh <siddhesh.mate@gmail.com>
2020-05-23 20:16:39 +0530

    v1.2.0

--- models/forumModel.js
+++ models/forumModel.js
@@ -6,7 +6,7 @@ const forumSchema = new mongoose.Schema({
         required: true,
         minlength: 2,
         maxlength: 255,
-        unique: true
+        
     },
     description: {
         type: String 
